a39442a4f96d37f9556b3236316d7e5eb7ab49be
rerro <101159509+rorre0@users.noreply.github.com>
2023-08-10 09:27:41 +0900

    3일차 리액트 학습
    - 모듈화

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import RootLayout from "./layout/RootLayout";
+
+import Home from "./pages/Home";
+// import About from "./pages/About";
+// {/* 홈 페이지: index.html */}
+// {/* 소개 페이지: about.html */}
+// {/* 제품 목록 페이지:product.html */}
+// {/* 의뢰 페이지: contact.html */}
+
+function App() {
+  return (
+    <div className="App">
+      <RootLayout></RootLayout>
+    </div>
+  );
+}
+
+export default App;
